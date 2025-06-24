cc7d32bd0646c578cbb9ce38f51300b28f7a4fc8
Aditya Rao <adityarao9541@gmail.com>
2025-06-24 18:00:41 +0530

    Updates

---
+++ precache-manifest.16cf7aadb151cd2e0bf6840be4008206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54244a1497f8256cf0919975eaba870",
+    "url": "/Mentolink/index.html"
+  },
+  {
+    "revision": "96be2b7af3e67f4b7abb",
+    "url": "/Mentolink/static/css/main.6071a9be.chunk.css"
+  },
+  {
+    "revision": "d9fb16b60c1c965cf967",
+    "url": "/Mentolink/static/js/2.a6e093a3.chunk.js"
+  },
+  {
+    "revision": "380c4a8dac873034c470efca9134b09c",
+    "url": "/Mentolink/static/js/2.a6e093a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96be2b7af3e67f4b7abb",
+    "url": "/Mentolink/static/js/main.46596353.chunk.js"
+  },
+  {
+    "revision": "8f9e3e5e0684e8452e46",
+    "url": "/Mentolink/static/js/runtime-main.c4ec335a.js"
+  }
+]);
